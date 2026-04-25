eaf0f1b42918816cdb068c90f49b63ed2ab7c100
André Mattos <andrexpsouza@gmail.com>
2026-04-24 22:24:05 -0300

    Update tempo_execucao.h

--- include/tempo_execucao.h
+++ include/tempo_execucao.h
@@ -2,7 +2,6 @@
 #define TEMPO_EXECUCAO_H
 
 void iniciar_timer(); // inicia um temporizador 
-double finalizar_timer(); // finaliza o temporizador retornando o tempo gasto
-
+double finalizar_timer(); // finaliza o tempo de execução
 
 #endif
